0aabdc4f0f194254e590e080310c56fc905ef828
BastLast <lastaccount@fastmail.com>
2025-05-25 22:34:28 +0200

    First work #3327

--- Lib/src/CrowniclesIcons.ts
+++ Lib/src/CrowniclesIcons.ts
@@ -946,6 +946,13 @@ export const CrowniclesIcons: {
 			stepAside: "👋",
 			help: "🤝"
 		},
+		100: {
+			end: {
+				0: "👀"
+			},
+			cutTree: "🐐",
+			otherWay: "🏃‍♂️"
+		},
 		8: {
 			end: {
 				0: "🚶"
