6560f6679ecac3b7fddd847147ee8055801262fa
BastLast <lastaccount@fastmail.com>
2025-07-01 01:21:21 +0200

    fix deepsource
    update lockfile for lib
    
    update dep for RestWs
    
    eslint fix RestWs
    
    lockfile update RestWs
    
    whyyyy did we cache the lock file ??? lol

--- Discord/src/utils/DiscordItemUtils.ts
+++ Discord/src/utils/DiscordItemUtils.ts
@@ -182,7 +182,7 @@ export class DiscordItemUtils {
 			name: i18n.t(`models:${model}.${displayPacket.id}`, {
 				lng
 			}),
-			emote: emote,
+			emote,
 			rarity: i18n.t(`items:rarities.${displayPacket.rarity}`, { lng }),
 			values
 		});
