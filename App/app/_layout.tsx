63a4d3bbd8da0e12b30f6072202c8caf960f3bac
Feiryn <98115666+feiryn@users.noreply.github.com>
2025-07-18 22:12:56 +0200

    Login page and protected other pages if not connected

--- App/app/_layout.tsx
+++ App/app/_layout.tsx
@@ -1,5 +1,24 @@
-import { Stack } from "expo-router";
+import {Stack} from "expo-router";
+import {StatusBar} from "expo-status-bar";
+import React from "react";
+import {AuthProvider} from "@/src/authentication/AuthContext";
 
 export default function RootLayout() {
-  return <Stack />;
+	return <AuthProvider>
+		<StatusBar style="auto" />
+		<Stack
+			screenOptions={{
+				headerShown: false,
+				contentStyle: { backgroundColor: "white" },
+			}}
+		>
+			<Stack.Screen name="(protected)" options={{
+				headerShown: false,
+				animation: "none"
+			}} />
+			<Stack.Screen name="login" options={{
+				animation: "none"
+			}}/>
+		</Stack>
+	</AuthProvider>;
 }
