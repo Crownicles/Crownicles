ea1bcc1adfc769bc8075c80f57dd4e6aa24fc906
Feiryn <98115666+feiryn@users.noreply.github.com>
2025-09-01 13:24:56 +0200

    Add travels to skip cities

--- Core/src/data/City.ts
+++ Core/src/data/City.ts
@@ -20,13 +20,35 @@ export class CityInn {
 export class City extends Data<string> {
 	public readonly mapLinks: number[];
 
-	public readonly inns: string[];
+	public readonly maps: number[];
+
+	public readonly inns: CityInn[];
+
+	public getTodayInnMeals(inn: CityInn): InnMeal[] {
+		let seed = new Date().getDate();
+		seed += new Date().getMonth() * 100;
+		seed += new Date().getFullYear() * 10000;
+
+		const meals: InnMeal[] = [];
+
+		let availableMeals = [...inn.meals];
+		while (meals.length < 3 && availableMeals.length > 0) {
+			const index = (seed * 9301 + 49297) % 233280 % availableMeals.length;
+			meals.push(availableMeals[index]);
+			availableMeals = availableMeals.filter((_, i) => i !== index);
+			seed += 1;
+		}
+
+		return meals;
+	}
 }
 
 export class CityDataController extends DataControllerString<City> {
 	static readonly instance: CityDataController = new CityDataController("cities");
 
 	static mapLinksCache: Map<number, City> = null;
+
+	static mapCache: Map<number, City> = null;
 
 	newInstance(): City {
 		return new City();
@@ -39,12 +61,30 @@ export class CityDataController extends DataControllerString<City> {
 		return CityDataController.mapLinksCache.get(mapLinkId);
 	}
 
+	getCityByMapId(mapId: number): City | undefined {
+		if (!CityDataController.mapCache) {
+			this.initMapCache();
+		}
+		return CityDataController.mapCache.get(mapId);
+	}
+
 	private initMapLinksCache(): void {
 		if (!CityDataController.mapLinksCache) {
 			CityDataController.mapLinksCache = new Map<number, City>();
 			for (const city of this.data.values()) {
 				for (const link of city.mapLinks) {
 					CityDataController.mapLinksCache.set(link, city);
+				}
+			}
+		}
+	}
+
+	private initMapCache(): void {
+		if (!CityDataController.mapCache) {
+			CityDataController.mapCache = new Map<number, City>();
+			for (const city of this.data.values()) {
+				for (const map of city.maps) {
+					CityDataController.mapCache.set(map, city);
 				}
 			}
 		}
