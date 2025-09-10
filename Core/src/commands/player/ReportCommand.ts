DraftBot-A-Discord-Adventure/DraftBot

Show on destinations choice that the player will enter a city #3401

Core/src/commands/player/ReportCommand.ts
@@ -465,7 +465,8 @@ async function chooseDestination(
 		return {
 			mapId,
 			mapTypeId,
-			tripDuration: isPveMap || RandomUtils.draftbotRandom.bool() ? mapLink.tripDuration : null
+			tripDuration: isPveMap || RandomUtils.draftbotRandom.bool() ? mapLink.tripDuration : null,
+			enterInCity: Boolean(CityDataController.instance.getCityByMapLinkId(mapLink.id))
 		};
 	});
 

Crownicles/Crownicles

Core/src/commands/player/ReportCommand.ts
@@ -465,7 +465,8 @@ async function chooseDestination(
 		return {
 			mapId,
 			mapTypeId,
-			tripDuration: isPveMap || RandomUtils.crowniclesRandom.bool() ? mapLink.tripDuration : null
+			tripDuration: isPveMap || RandomUtils.crowniclesRandom.bool() ? mapLink.tripDuration : null,
+			enterInCity: Boolean(CityDataController.instance.getCityByMapLinkId(mapLink.id))
 		};
 	});
 