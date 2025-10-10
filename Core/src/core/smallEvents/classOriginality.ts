423aa0bf132f553ef730ccb2580b2a6b383ef215
Ntalcme <57364571+Ntalcme@users.noreply.github.com>
2025-10-10 02:48:56 +0200

    end core SE classOriginality #2725

--- Core/src/core/smallEvents/classOriginality.ts
+++ Core/src/core/smallEvents/classOriginality.ts
@@ -1,12 +1,19 @@
 import { SmallEventFuncs } from "../../data/SmallEvent";
 import { Maps } from "../maps/Maps";
 import { ClassDataController } from "../../data/Class";
-import { Players } from "../database/game/models/Player";
+import {
+	Player,
+	Players
+} from "../database/game/models/Player";
 import { makePacket } from "../../../../Lib/src/packets/CrowniclesPacket";
 import { SmallEventClassOriginalityPacket } from "../../../../Lib/src/packets/smallEvents/SmallEventClassOriginalityPacket";
+import { PlayerSmallEvents } from "../database/game/models/PlayerSmallEvent";
 
 export const smallEventFuncs: SmallEventFuncs = {
-	canBeExecuted: player => Maps.isOnContinent(player),
+	async canBeExecuted(player: Player): Promise<boolean> {
+		return Maps.isOnContinent(player)
+				&& await PlayerSmallEvents.playerSmallEventCount(player.id, "classOriginality") <= 3;
+	},
 	executeSmallEvent: async (response, player, _context): Promise<void> => {
 		const currentClassGroup = ClassDataController.instance.getById(player.class).classGroup;
 		const leastCommonClassId = await Players.getLeastCommonClassIdForTier(currentClassGroup);
