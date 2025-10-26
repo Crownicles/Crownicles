b12340b3844f70b85d4b4a4f4c5ae6f013dac3ae
Ntalcme <57364571+Ntalcme@users.noreply.github.com>
2025-10-26 17:03:43 +0100

    se packet #3689

---
+++ Lib/src/packets/smallEvents/SmallEventHauntedPacket.ts
@@ -0,0 +1,7 @@
+import { SmallEventPacket } from "./SmallEventPacket";
+import {
+	PacketDirection, sendablePacket
+} from "../CrowniclesPacket";
+
+@sendablePacket(PacketDirection.BACK_TO_FRONT)
+export class SmallEventHauntedPacket extends SmallEventPacket {}
