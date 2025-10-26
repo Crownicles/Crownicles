4bac06b2d08435dd79e627b2569085f62ec95f16
Ntalcme <57364571+Ntalcme@users.noreply.github.com>
2025-10-26 17:02:56 +0100

    add new management for event outcomes #3689

---
+++ Core/src/data/events/PossibilityOutcomeCondition.ts
@@ -0,0 +1,31 @@
+import Player from "../../core/database/game/models/Player";
+import Guild, { Guilds } from "../../core/database/game/models/Guild";
+import { GuildPets } from "../../core/database/game/models/GuildPet";
+
+export interface PossibilityOutcomeCondition {
+	canAcceptPet?: boolean;
+}
+
+async function verifyConditionCanAcceptPet(condition: PossibilityOutcomeCondition, player: Player): Promise<boolean> {
+	if (!condition.canAcceptPet) {
+		return true;
+	}
+
+	let guild: Guild;
+
+	// Search for a user's guild
+	try {
+		guild = await Guilds.getById(player.guildId);
+	}
+	catch {
+		guild = null;
+	}
+
+	const noRoomInGuild = !guild ? true : guild.isPetShelterFull(await GuildPets.getOfGuild(guild.id));
+
+	return !(noRoomInGuild && player.petId !== null);
+}
+
+export async function verifyPossibilityOutcomeCondition(condition: PossibilityOutcomeCondition, player: Player): Promise<boolean> {
+	return await verifyConditionCanAcceptPet(condition, player);
+}
