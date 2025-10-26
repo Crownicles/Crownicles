9eb0d4025cbe86d2cb1e53b903ddd2989923d96c
Ntalcme <57364571+Ntalcme@users.noreply.github.com>
2025-10-26 17:02:39 +0100

    add new power for pets #3689

---
+++ Core/src/core/fights/actions/interfaces/pets/petMakeBleed.ts
@@ -0,0 +1,47 @@
+import { FightActionController } from "../../FightActionController";
+import { PetAssistanceFunc } from "../../../../../data/PetAssistance";
+import {
+	PetAssistanceResult, PetAssistanceState
+} from "../../../../../../../Lib/src/types/PetAssistanceResult";
+import { FightAlterations } from "../../FightAlterations";
+import { FightUtils } from "../../../../utils/FightUtils";
+import {FightStatBuffed} from "../../../../../../../Lib/src/types/FightActionResult";
+import {FightStatModifierOperation} from "../../../../../../../Lib/src/types/FightStatModifierOperation";
+import {RandomUtils} from "../../../../../../../Lib/src/utils/RandomUtils";
+import {Fighter} from "../../../fighter/Fighter";
+
+function makeBleed(opponent: Fighter, result: PetAssistanceResult): void {
+	// Make bleed the opponent
+	FightActionController.applyAlteration(result, {
+		selfTarget: false,
+		alteration: FightAlterations.BLEEDING
+	}, opponent);
+}
+
+const use: PetAssistanceFunc = (fighter, opponent, turn, _fightController): Promise<PetAssistanceResult | null> => {
+	if (turn % 3 !== 0) {
+		return null;
+	}
+	const result: PetAssistanceResult = {
+		assistanceStatus: PetAssistanceState.SUCCESS
+	};
+
+	if (!opponent.hasFightAlteration()) {
+		makeBleed(opponent, result);
+	}
+
+	const pointsToHealFighter = fighter.getMaxEnergy() - fighter.getEnergy();
+
+	if (pointsToHealFighter > 0) {
+		FightActionController.applyBuff(result, {
+			selfTarget: true,
+			stat: FightStatBuffed.ENERGY,
+			operator: FightStatModifierOperation.ADDITION,
+			value: RandomUtils.crowniclesRandom.integer(15, Math.max(fighter.getMaxEnergy() * 0.07, 20))
+		}, fighter, this);
+	}
+
+	return Promise.resolve(result);
+};
+
+export default use;
---
+++ Core/src/core/fights/actions/interfaces/pets/petPetrified.ts
@@ -0,0 +1,30 @@
+import { FightActionController } from "../../FightActionController";
+import { PetAssistanceFunc } from "../../../../../data/PetAssistance";
+import {
+	PetAssistanceResult, PetAssistanceState
+} from "../../../../../../../Lib/src/types/PetAssistanceResult";
+import { FightAlterations } from "../../FightAlterations";
+import { Fighter } from "../../../fighter/Fighter";
+
+const shouldSkipPetPetrified = (turn: number, opponent: Fighter): boolean => {
+	return (turn !== 10 && turn !== 11) || opponent.hasFightAlteration();
+};
+
+const use: PetAssistanceFunc = (_fighter, opponent, turn, _fightController): Promise<PetAssistanceResult | null> => {
+	if (shouldSkipPetPetrified(turn, opponent)) {
+		return null;
+	}
+
+	const result: PetAssistanceResult = {
+		assistanceStatus: PetAssistanceState.SUCCESS
+	};
+
+	FightActionController.applyAlteration(result, {
+		selfTarget: false,
+		alteration: FightAlterations.PETRIFIED
+	}, opponent);
+
+	return Promise.resolve(result);
+};
+
+export default use;
