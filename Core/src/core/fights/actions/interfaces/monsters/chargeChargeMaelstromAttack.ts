959c500b0a8a0385656667359b6e8d70f7994833
Ntalcme <57364571+Ntalcme@users.noreply.github.com>
2025-11-08 14:50:35 +0100

    add a max use for maelstrom attack and make it uncounterable #3726

--- Core/src/core/fights/actions/interfaces/monsters/chargeChargeMaelstromAttack.ts
+++ Core/src/core/fights/actions/interfaces/monsters/chargeChargeMaelstromAttack.ts
@@ -1,10 +1,17 @@
 import {
 	FightActionDataController, FightActionFunc
 } from "../../../../../data/FightAction";
-import { customMessageActionResult } from "../../../../../../../Lib/src/types/FightActionResult";
+import {
+	customMessageActionResult,
+	defaultMaxUsesFightActionResult
+} from "../../../../../../../Lib/src/types/FightActionResult";
 import { FightConstants } from "../../../../../../../Lib/src/constants/FightConstants";
 
 const use: FightActionFunc = sender => {
+	const count = sender.fightActionsHistory.filter(action => action.id === FightConstants.FIGHT_ACTIONS.MONSTER.MAELSTROM_ATTACK).length;
+	if (count >= 1) {
+		return defaultMaxUsesFightActionResult();
+	}
 	sender.nextFightAction = FightActionDataController.instance.getById(FightConstants.FIGHT_ACTIONS.MONSTER.CHARGE_MAELSTROM_ATTACK);
 	return customMessageActionResult();
 };
