2d959a8db9d5194d37606a189e111a2900b34430
BastLast <lastaccount@fastmail.com>
2025-11-18 02:26:41 +0100

    fix eslint & deepsource

--- Core/src/core/smallEvents/fightPet/provoke.ts
+++ Core/src/core/smallEvents/fightPet/provoke.ts
@@ -6,6 +6,11 @@ import { PetUtils } from "../../utils/PetUtils";
 
 export const fightPetAction: FightPetActionFunc = async (player, pet) =>
 
-// Succeeds if the player has an attack greater than his level times the vigor of the enraged animal and if random > level / 100 OR if the player has the "insults" object in his inventory
-	player.getCumulativeAttack(await InventorySlots.getMainSlotsItems(player.id)) >= player.level * PetUtils.getPetVigor(pet, 0, { enraged: true }) && RandomUtils.crowniclesRandom.bool(1 - player.level / 100)
+	/**
+	 * Succeeds if the player has an attack greater than his level times the vigor of the enraged animal
+	 * and if random > level / 100 OR if the player has the "insults" object in his inventory
+	 */
+	player.getCumulativeAttack(await InventorySlots.getMainSlotsItems(player.id))
+		>= player.level * PetUtils.getPetVigor(pet, 0, { enraged: true })
+		&& RandomUtils.crowniclesRandom.bool(1 - player.level / 100)
 	|| await InventorySlots.hasItem(player.id, 83, ItemCategory.WEAPON);
