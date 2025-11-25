50dd5263222cba5be9c03cba0a02e11d43b9ddf8
BastLast <38099970+BastLast@users.noreply.github.com>
2025-11-25 09:15:35 +0100

    Fixes (#3794)

--- Discord/src/smallEvents/interactOtherPlayers.ts
+++ Discord/src/smallEvents/interactOtherPlayers.ts
@@ -13,6 +13,12 @@ import { keycloakConfig } from "../bot/CrowniclesShard";
 import { Language } from "../../../Lib/src/Language";
 import { CrowniclesIcons } from "../../../Lib/src/CrowniclesIcons";
 import { ReactionCollectorReturnTypeOrNull } from "../packetHandlers/handlers/ReactionCollectorHandlers";
+import { CrowniclesInteraction } from "../messages/CrowniclesInteraction";
+import {
+	InteractOtherPlayerInteraction,
+	SmallEventInteractOtherPlayersPacket
+} from "../../../Lib/src/packets/smallEvents/SmallEventInteractOtherPlayers";
+import { DisplayUtils } from "../utils/DisplayUtils";
 
 export async function interactOtherPlayerGetPlayerDisplay(keycloakId: string, rank: number | undefined, lng: Language): Promise<string> {
 	const getUser = await KeycloakUtils.getUserByKeycloakId(keycloakConfig, keycloakId);
@@ -52,4 +58,80 @@ export async function interactOtherPlayersCollector(context: PacketContext, pack
 			}
 		}
 	);
+}
+
+/**
+ * Handles the case where no player is found for the interaction
+ * @param interaction
+ * @param lng
+ */
+export async function handleNoPlayerInteraction(interaction: CrowniclesInteraction, lng: Language): Promise<void> {
+	await interaction.editReply({
+		embeds: [
+			new CrowniclesSmallEventEmbed(
+				"interactOtherPlayers",
+				StringUtils.getRandomTranslation("smallEvents:interactOtherPlayers.no_one", lng),
+				interaction.user,
+				lng
+			)
+		]
+	});
+}
+
+/**
+ * Handles the case where the interaction is an effect
+ * @param interaction
+ * @param packet
+ * @param lng
+ * @param playerDisplay
+ */
+export async function handleEffectInteraction(interaction: CrowniclesInteraction, packet: SmallEventInteractOtherPlayersPacket, lng: Language, playerDisplay: string): Promise<void> {
+	await interaction.editReply({
+		embeds: [
+			new CrowniclesSmallEventEmbed(
+				"interactOtherPlayers",
+				StringUtils.getRandomTranslation(`smallEvents:interactOtherPlayers.effect.${packet.data!.effectId}`, lng, { playerDisplay }),
+				interaction.user,
+				lng
+			)
+		]
+	});
+}
+
+/**
+ * Handles the case where the interaction is not an effect
+ * @param interaction
+ * @param packet
+ * @param lng
+ * @param playerDisplay
+ */
+export async function handleOtherInteractions(interaction: CrowniclesInteraction, packet: SmallEventInteractOtherPlayersPacket, lng: Language, playerDisplay: string): Promise<void> {
+	const hasPetInfo = packet.data!.petId && packet.data!.petSex;
+	await interaction.editReply({
+		embeds: [
+			new CrowniclesSmallEventEmbed(
+				"interactOtherPlayers",
+				StringUtils.getRandomTranslation(
+					`smallEvents:interactOtherPlayers.${InteractOtherPlayerInteraction[packet.playerInteraction!].toLowerCase()}`,
+					lng,
+					{
+						playerDisplay,
+						level: packet.data!.level,
+						class: DisplayUtils.getClassDisplay(packet.data!.classId, lng),
+						classPlural: DisplayUtils.getClassDisplay(packet.data!.classId, lng, true),
+						advice: StringUtils.getRandomTranslation("advices:advices", lng),
+						petEmote: hasPetInfo ? DisplayUtils.getPetIcon(packet.data!.petId!, packet.data!.petSex!) : "",
+						petName: hasPetInfo ? DisplayUtils.getPetNicknameOrTypeName(packet.data!.petName ?? null, packet.data!.petId!, packet.data!.petSex!, lng) : "",
+						guildName: packet.data!.guildName,
+						weapon: DisplayUtils.getWeaponDisplay(packet.data!.weaponId, lng),
+						armor: DisplayUtils.getArmorDisplay(packet.data!.armorId, lng),
+						object: DisplayUtils.getObjectDisplay(packet.data!.objectId, lng),
+						potion: DisplayUtils.getPotionDisplay(packet.data!.potionId, lng)
+					}
+				),
+				interaction.user,
+				lng
+			)
+		]
+	});
 }
