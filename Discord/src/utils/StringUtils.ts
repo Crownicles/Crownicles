2da24cd4075430d52ef3dc45ce0f163254aab766
BastLast <lastaccount@fastmail.com>
2025-12-02 21:16:55 +0100

    eslint discord

--- Discord/src/utils/StringUtils.ts
+++ Discord/src/utils/StringUtils.ts
@@ -17,8 +17,11 @@ export class StringUtils {
 	static getRandomTranslation(tr: string, lng: Language, replacements: { [key: string]: unknown } = {}): string {
 		// If context is provided, append it to the translation key for array translations
 		const translationKey = replacements.context ? `${tr}_${replacements.context}` : tr;
+
 		// Remove context from replacements since it was already used to build the translation key
-		const { context: _, ...remainingReplacements } = replacements;
+		const {
+			context: _, ...remainingReplacements
+		} = replacements;
 		const intros: string[] = i18n.t(translationKey, {
 			returnObjects: true,
 			lng,
