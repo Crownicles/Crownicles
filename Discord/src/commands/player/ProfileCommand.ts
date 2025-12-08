9a6a4a63cc2e00c601f78a32c3886a609d837da2
BastLast <lastaccount@fastmail.com>
2025-12-08 01:54:35 +0100

    Refactorisation de la génération des champs du profil : ajout des champs de statistiques, de rang et d'effet, ainsi que des champs de localisation. Introduction d'une fonction pour gérer les réactions aux badges.

--- Discord/src/commands/player/ProfileCommand.ts
+++ Discord/src/commands/player/ProfileCommand.ts
@@ -12,7 +12,7 @@ import {
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { CrowniclesEmbed } from "../../messages/CrowniclesEmbed";
 import {
-	ColorResolvable, EmbedField, Message, MessageReaction
+	ColorResolvable, EmbedField, Message, MessageReaction, ReactionCollector
 } from "discord.js";
 import { Constants } from "../../../../Lib/src/constants/Constants";
 import { DiscordCache } from "../../bot/DiscordCache";
@@ -155,15 +155,9 @@ function addPetField(fields: EmbedField[], packet: CommandProfilePacketRes, lng:
 }
 
 /**
- * Generate the fields of the profile embed
- * @param packet
- * @param lng
+ * Add statistics field
  */
-function generateFields(packet: CommandProfilePacketRes, lng: Language): EmbedField[] {
-	const fields: EmbedField[] = [];
-
-	addInformationField(fields, packet, lng);
-
+function addStatisticsField(fields: EmbedField[], packet: CommandProfilePacketRes, lng: Language): void {
 	addField(fields, "statistics", Boolean(packet.playerData.stats), {
 		lng,
 		baseBreath: packet.playerData.stats?.breath.base,
@@ -175,6 +169,61 @@ function generateFields(packet: CommandProfilePacketRes, lng: Language): EmbedFi
 		cumulativeMaxHealth: packet.playerData.stats?.energy.max,
 		maxBreath: packet.playerData.stats?.breath.max
 	});
+}
+
+/**
+ * Add rank and effect fields
+ */
+function addRankAndEffectFields(fields: EmbedField[], packet: CommandProfilePacketRes, lng: Language): void {
+	const rankFieldKey = packet.playerData.rank.unranked ? "unranked" : "ranking";
+	addField(fields, rankFieldKey, true, {
+		lng,
+		score: packet.playerData.rank.score,
+		rank: packet.playerData.rank.rank,
+		numberOfPlayer: packet.playerData.rank.numberOfPlayers
+	});
+
+	const effectFieldKey = packet.playerData.effect.healed ? "noTimeLeft" : "timeLeft";
+	addField(fields, effectFieldKey, Boolean(packet.playerData.effect.hasTimeDisplay), {
+		lng,
+		effectId: packet.playerData.effect.effect,
+		timeLeft: minutesDisplay(millisecondsToMinutes(packet.playerData.effect.timeLeft), lng)
+	});
+}
+
+/**
+ * Add location fields (class, guild, map)
+ */
+function addLocationFields(fields: EmbedField[], packet: CommandProfilePacketRes, lng: Language): void {
+	const hasClass = Boolean(packet.playerData.classId) || packet.playerData.classId === 0;
+	addField(fields, "playerClass", hasClass, {
+		lng,
+		id: packet.playerData.classId
+	});
+
+	addField(fields, "guild", Boolean(packet.playerData.guild), {
+		lng,
+		guild: packet.playerData.guild
+	});
+
+	const hasMap = Boolean(packet.playerData.destinationId && packet.playerData.mapTypeId);
+	addField(fields, "map", hasMap, {
+		lng,
+		mapTypeId: packet.playerData.mapTypeId,
+		mapName: packet.playerData.destinationId
+	});
+}
+
+/**
+ * Generate the fields of the profile embed
+ * @param packet
+ * @param lng
+ */
+function generateFields(packet: CommandProfilePacketRes, lng: Language): EmbedField[] {
+	const fields: EmbedField[] = [];
+
+	addInformationField(fields, packet, lng);
+	addStatisticsField(fields, packet, lng);
 
 	addField(fields, "mission", true, {
 		lng,
@@ -182,37 +231,9 @@ function generateFields(packet: CommandProfilePacketRes, lng: Language): EmbedFi
 		campaign: packet.playerData.missions.campaignProgression
 	});
 
-	addField(fields, packet.playerData.rank.unranked ? "unranked" : "ranking", true, {
-		lng,
-		score: packet.playerData.rank.score,
-		rank: packet.playerData.rank.rank,
-		numberOfPlayer: packet.playerData.rank.numberOfPlayers
-	});
-
-	addField(fields, packet.playerData.effect.healed ? "noTimeLeft" : "timeLeft", Boolean(packet.playerData.effect.hasTimeDisplay), {
-		lng,
-		effectId: packet.playerData.effect.effect,
-		timeLeft: minutesDisplay(millisecondsToMinutes(packet.playerData.effect.timeLeft), lng)
-	});
-
-	addField(fields, "playerClass", Boolean(packet.playerData.classId) || packet.playerData.classId === 0, {
-		lng,
-		id: packet.playerData.classId
-	});
-
+	addRankAndEffectFields(fields, packet, lng);
+	addLocationFields(fields, packet, lng);
 	addFightRankingField(fields, packet, lng);
-
-	addField(fields, "guild", Boolean(packet.playerData.guild), {
-		lng,
-		guild: packet.playerData.guild
-	});
-
-	addField(fields, "map", Boolean(packet.playerData.destinationId && packet.playerData.mapTypeId), {
-		lng,
-		mapTypeId: packet.playerData.mapTypeId,
-		mapName: packet.playerData.destinationId
-	});
-
 	addPetField(fields, packet, lng);
 
 	return fields;
@@ -223,6 +244,32 @@ function generateFields(packet: CommandProfilePacketRes, lng: Language): EmbedFi
  * @param packet
  * @param context
  */
+/**
+ * Handle badge reaction collection
+ */
+function handleBadgeReaction(
+	reaction: MessageReaction,
+	collector: ReactionCollector,
+	pseudo: string,
+	badges: Badge[],
+	interaction: CrowniclesInteraction,
+	lng: Language
+): void {
+	if (reaction.emoji.name === CrowniclesIcons.profile.displayAllBadgeEmote) {
+		collector.stop(); // Only one is allowed to avoid spam
+		void sendMessageAllBadgesTooMuchBadges(pseudo, badges, interaction);
+		return;
+	}
+
+	const badge = Object.entries(CrowniclesIcons.badges).find(badgeEntry => badgeEntry[1] === reaction.emoji.name);
+	if (badge) {
+		interaction.channel.send({ content: `\`${reaction.emoji.name!} ${i18n.t(`commands:profile.badges.${badge[0]}`, { lng })}\`` })
+			.then((msg: Message | null) => {
+				setTimeout(() => msg?.delete(), ProfileConstants.BADGE_DESCRIPTION_TIMEOUT);
+			});
+	}
+}
+
 export async function handleCommandProfilePacketRes(packet: CommandProfilePacketRes, context: PacketContext): Promise<void> {
 	const interaction = DiscordCache.getInteraction(context.discord!.interaction);
 
@@ -256,20 +303,8 @@ export async function handleCommandProfilePacketRes(packet: CommandProfilePacket
 		time: Constants.MESSAGES.COLLECTOR_TIME,
 		max: ProfileConstants.BADGE_MAXIMUM_REACTION
 	});
-	collector.on("collect", async reaction => {
-		if (reaction.emoji.name === CrowniclesIcons.profile.displayAllBadgeEmote) {
-			collector.stop(); // Only one is allowed to avoid spam
-			await sendMessageAllBadgesTooMuchBadges(pseudo, packet.playerData!.badges!, interaction);
-		}
-		else {
-			const badge = Object.entries(CrowniclesIcons.badges).find(badgeEntry => badgeEntry[1] === reaction.emoji.name);
-			if (badge) {
-				interaction.channel.send({ content: `\`${reaction.emoji.name!} ${i18n.t(`commands:profile.badges.${badge[0]}`, { lng })}\`` })
-					.then((msg: Message | null) => {
-						setTimeout(() => msg?.delete(), ProfileConstants.BADGE_DESCRIPTION_TIMEOUT);
-					});
-			}
-		}
+	collector.on("collect", reaction => {
+		handleBadgeReaction(reaction, collector, pseudo, packet.playerData!.badges!, interaction, lng);
 	});
 	if (packet.playerData?.badges.length !== 0) {
 		await displayBadges(packet.playerData!.badges, message);
