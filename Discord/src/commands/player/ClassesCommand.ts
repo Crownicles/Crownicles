1c4b067f7efd9cd9619033b3b6a305be5d79e330
Ntalcme <57364571+Ntalcme@users.noreply.github.com>
2025-12-11 20:29:40 +0100

    feat(collector): add typed packet for ChangeClass

--- Discord/src/commands/player/ClassesCommand.ts
+++ Discord/src/commands/player/ClassesCommand.ts
@@ -11,12 +11,11 @@ import { DiscordCache } from "../../bot/DiscordCache";
 import i18n from "../../translations/i18n";
 import { CrowniclesEmbed } from "../../messages/CrowniclesEmbed";
 import {
-	ReactionCollectorCreationPacket,
 	ReactionCollectorRefuseReaction
 } from "../../../../Lib/src/packets/interaction/ReactionCollectorPacket";
 import { ReactionCollectorReturnTypeOrNull } from "../../packetHandlers/handlers/ReactionCollectorHandlers";
 import {
-	ReactionCollectorChangeClassData,
+	ReactionCollectorChangeClassPacket,
 	ReactionCollectorChangeClassReaction
 } from "../../../../Lib/src/packets/interaction/ReactionCollectorChangeClass";
 import { DisplayUtils } from "../../utils/DisplayUtils";
@@ -78,7 +77,7 @@ export async function handleCommandClassesChangeSuccessPacket(packet: CommandCla
 	});
 }
 
-export async function handleChangeClassReactionCollector(context: PacketContext, packet: ReactionCollectorCreationPacket): Promise<ReactionCollectorReturnTypeOrNull> {
+export async function handleChangeClassReactionCollector(context: PacketContext, packet: ReactionCollectorChangeClassPacket): Promise<ReactionCollectorReturnTypeOrNull> {
 	const interaction = DiscordCache.getInteraction(context.discord!.interaction);
 
 	if (!interaction) {
@@ -86,7 +85,7 @@ export async function handleChangeClassReactionCollector(context: PacketContext,
 	}
 	const lng = interaction.userLanguage;
 
-	const data = packet.data.data as ReactionCollectorChangeClassData;
+	const data = packet.data.data;
 	const classesReactions = packet.reactions.filter(reaction => reaction.type === ReactionCollectorChangeClassReaction.name)
 		.map(reaction => reaction.data) as ReactionCollectorChangeClassReaction[];
 
