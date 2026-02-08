6bfbe29884d843c6f5488f9348f1781ef92f7d51
BastLast <lastaccount@fastmail.com>
2026-02-08 11:50:30 +0100

    format travel summary points for better readability

--- Discord/src/commands/player/ReportCommand.ts
+++ Discord/src/commands/player/ReportCommand.ts
@@ -731,7 +731,7 @@ export async function reportTravelSummary(packet: CommandReportTravelSummaryRes,
 	if (packet.points.show) {
 		travelEmbed.addFields({
 			name: i18n.t("commands:report.collectedPointsTitle", { lng }),
-			value: `${CrowniclesIcons.unitValues.score} ${packet.points.cumulated}`,
+			value: `${CrowniclesIcons.unitValues.score} ${DisplayUtils.formatNumber(packet.points.cumulated, lng)}`,
 			inline: true
 		});
 	}
