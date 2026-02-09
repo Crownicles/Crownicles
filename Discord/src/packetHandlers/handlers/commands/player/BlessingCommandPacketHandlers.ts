218e6d0501cd2926bd7451455af05b30f7f74564
BastLast <lastaccount@fastmail.com>
2026-02-09 23:04:51 +0100

    add new advices for altar/blessing/petDropToken + align blessing handler style

--- Discord/src/packetHandlers/handlers/commands/player/BlessingCommandPacketHandlers.ts
+++ Discord/src/packetHandlers/handlers/commands/player/BlessingCommandPacketHandlers.ts
@@ -3,93 +3,11 @@ import {
 	PacketContext
 } from "../../../../../../Lib/src/packets/CrowniclesPacket";
 import { CommandBlessingPacketRes } from "../../../../../../Lib/src/packets/commands/CommandBlessingPacketRes";
-import { DiscordCache } from "../../../../bot/DiscordCache";
-import { CrowniclesEmbed } from "../../../../messages/CrowniclesEmbed";
-import i18n from "../../../../translations/i18n";
-import { escapeUsername } from "../../../../utils/StringUtils";
-import { BlessingType } from "../../../../../../Lib/src/constants/BlessingConstants";
-import { KeycloakUtils } from "../../../../../../Lib/src/keycloak/KeycloakUtils";
-import { keycloakConfig } from "../../../../bot/CrowniclesShard";
-import { CrowniclesIcons } from "../../../../../../Lib/src/CrowniclesIcons";
-import { printTimeBeforeDate } from "../../../../../../Lib/src/utils/TimeUtils";
-import { progressBar } from "../../../../../../Lib/src/utils/StringUtils";
-import { Language } from "../../../../../../Lib/src/Language";
-
-/**
- * Resolve a keycloak ID to a player display name, with a fallback for unknown players
- */
-async function resolveKeycloakPlayerName(keycloakId: string | undefined | null, lng: Language): Promise<string> {
-	if (!keycloakId) {
-		return i18n.t("error:unknownPlayer", { lng });
-	}
-	const getUser = await KeycloakUtils.getUserByKeycloakId(keycloakConfig, keycloakId);
-	if (!getUser.isError && getUser.payload.user.attributes.gameUsername) {
-		return escapeUsername(getUser.payload.user.attributes.gameUsername[0]);
-	}
-	return i18n.t("error:unknownPlayer", { lng });
-}
-
-/**
- * Build the top contributor line for the collecting state, or empty string if no contributors
- */
-async function buildTopContributorLine(packet: CommandBlessingPacketRes, lng: Language): Promise<string> {
-	if (!packet.topContributorKeycloakId || packet.totalContributors <= 0) {
-		return "";
-	}
-	const topContributorName = await resolveKeycloakPlayerName(packet.topContributorKeycloakId, lng);
-	return `\n\n${i18n.t("commands:blessing.contributors", {
-		lng,
-		topContributorName,
-		topContributorAmount: packet.topContributorAmount,
-		totalContributors: packet.totalContributors,
-		moneyEmote: CrowniclesIcons.unitValues.money
-	})}`;
-}
+import { handleCommandBlessingPacketRes } from "../../../../commands/player/BlessingCommand";
 
 export default class BlessingCommandPacketHandlers {
 	@packetHandler(CommandBlessingPacketRes)
 	async blessingRes(context: PacketContext, packet: CommandBlessingPacketRes): Promise<void> {
-		const interaction = DiscordCache.getInteraction(context.discord!.interaction);
-		if (!interaction) {
-			return;
-		}
-
-		const lng = interaction.userLanguage;
-		const hasBlessing = packet.activeBlessingType !== BlessingType.NONE;
-
-		let description: string;
-
-		if (hasBlessing) {
-			const triggeredByName = await resolveKeycloakPlayerName(packet.lastTriggeredByKeycloakId, lng);
-
-			description = i18n.t("commands:blessing.active", {
-				lng,
-				blessingName: i18n.t(`bot:blessingNames.${packet.activeBlessingType}`, { lng }),
-				blessingEffect: i18n.t(`bot:blessingEffects.${packet.activeBlessingType}`, { lng }),
-				timeLeft: printTimeBeforeDate(packet.blessingEndAt),
-				triggeredBy: triggeredByName,
-				moneyEmote: CrowniclesIcons.unitValues.money
-			});
-		}
-		else {
-			const topContributorLine = await buildTopContributorLine(packet, lng);
-
-			description = `${i18n.t("commands:blessing.collecting", {
-				lng,
-				poolAmount: packet.poolAmount,
-				poolThreshold: packet.poolThreshold,
-				moneyEmote: CrowniclesIcons.unitValues.money,
-				percentage: Math.floor(packet.poolAmount / packet.poolThreshold * 100)
-			})}\n${progressBar(packet.poolAmount, packet.poolThreshold)}${topContributorLine}`;
-		}
-
-		const embed = new CrowniclesEmbed()
-			.formatAuthor(i18n.t("commands:blessing.title", {
-				lng,
-				pseudo: escapeUsername(interaction.user.displayName)
-			}), interaction.user)
-			.setDescription(description);
-
-		await interaction.editReply({ embeds: [embed] });
+		await handleCommandBlessingPacketRes(context, packet);
 	}
 }
