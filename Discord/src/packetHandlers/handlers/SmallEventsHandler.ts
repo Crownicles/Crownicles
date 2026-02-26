2813ac51c690d8cdc41a87e6e99b646ab4e2c526
BastLast <lastaccount@fastmail.com>
2026-02-26 22:49:02 +0100

    add farmer small event (Mylène) on plains #4041

--- Discord/src/packetHandlers/handlers/SmallEventsHandler.ts
+++ Discord/src/packetHandlers/handlers/SmallEventsHandler.ts
@@ -111,6 +111,7 @@ import {
 	SmallEventAltarFirstEncounterPacket,
 	SmallEventAltarNoContributionPacket
 } from "../../../../Lib/src/packets/smallEvents/SmallEventAltarPacket";
+import { SmallEventFarmerPacket } from "../../../../Lib/src/packets/smallEvents/SmallEventFarmerPacket";
 import {
 	altarContributed, altarFirstEncounter, altarNoContribution
 } from "../../smallEvents/altar";
@@ -1236,5 +1237,25 @@ export default class SmallEventsHandler {
 	@packetHandler(SmallEventAltarContributedPacket)
 	async smallEventAltarContributed(context: PacketContext, packet: SmallEventAltarContributedPacket): Promise<void> {
 		await altarContributed(packet, context);
+	}
+
+	@packetHandler(SmallEventFarmerPacket)
+	async smallEventFarmer(context: PacketContext, packet: SmallEventFarmerPacket): Promise<void> {
+		const interaction = DiscordCache.getInteraction(context.discord!.interaction);
+		const lng = interaction!.userLanguage;
+		await interaction?.editReply({
+			embeds: [
+				new CrowniclesSmallEventEmbed(
+					"farmer",
+					getRandomSmallEventIntro(lng)
+					+ StringUtils.getRandomTranslation("smallEvents:farmer.stories", lng)
+					+ StringUtils.getRandomTranslation(`smallEvents:farmer.rewards.${packet.interactionName}`, lng, {
+						count: packet.amount
+					}),
+					interaction.user,
+					lng
+				)
+			]
+		});
 	}
 }
