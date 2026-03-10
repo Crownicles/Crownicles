d4a96fca5a85ea6827f7d4fd044966039e6a7e1d
romain22222 <romrom22222@hotmail.fr>
2026-03-10 12:26:17 +0100

    new top

---
+++ Core/src/core/utils/TopUtils.ts
@@ -0,0 +1,327 @@
+import {TopTiming} from "../../../../Lib/src/types/TopTimings";
+import {TopElementGlory, TopElementGuild, TopElementScore} from "../../../../Lib/src/types/TopElement";
+import {Players} from "../database/game/models/Player";
+import {TravelTime} from "../maps/TravelTime";
+import {Guilds} from "../database/game/models/Guild";
+import {TopDataType} from "../../../../Lib/src/types/TopDataType";
+import {CrowniclesPacket, makePacket} from "../../../../Lib/src/packets/CrowniclesPacket";
+import {
+	CommandTopGuildsEmptyPacket,
+	CommandTopInvalidPagePacket,
+	CommandTopPacketResGlory,
+	CommandTopPacketResGuild,
+	CommandTopPacketResScore,
+	CommandTopPlayersEmptyPacket
+} from "../../../../Lib/src/packets/commands/CommandTopPacket";
+
+type TopObject = {
+	totalElements: number,
+	timing: TopTiming,
+}
+
+type TopElementBaseStorage<Element> = Element & { id: number };
+
+type TopObjectStorage<Element> = TopObject & {
+	elements: TopElementBaseStorage<Element>[]
+}
+
+type TopObjectResponse<Element> = TopObject & {
+	elements: Element[],
+	contextRank?: number,
+	canBeRanked: boolean
+	minRank: number,
+	maxRank: number,
+	elementsPerPage: number,
+	needFight?: number
+}
+
+type AskTopResultBase = {
+	result: TopAskingResult,
+	kind: TopKind
+}
+
+type AskTopResultOKBase<Element, Kind extends TopKind> = AskTopResultBase & {
+	result: TopAskingResult.OK,
+	data: TopObjectResponse<Element>,
+	now: number,
+	kind: Kind
+}
+
+type AskTopResultOK<T extends TopKind> = AskTopResultOKBase<TopElementKind<T>, T>
+
+type AskTopResultNoElement = AskTopResultBase & {
+	result: TopAskingResult.NO_ELEMENT,
+	data: { needFight?: number }
+}
+
+type AskTopResultInvalidPage = AskTopResultBase & {
+	result: TopAskingResult.INVALID_PAGE,
+	data: { minPage: number, maxPage: number }
+}
+
+type AskTopResult<T extends TopKind> = AskTopResultOK<T> | AskTopResultNoElement | AskTopResultInvalidPage;
+
+type TopElementKind<Kind extends TopKind> = Kind extends TopKind.SCORE_ALL_TIME | TopKind.SCORE_WEEKLY
+	? TopElementScore
+	: Kind extends TopKind.GLORY
+		? TopElementGlory
+		: Kind extends TopKind.GUILDS
+			? TopElementGuild
+			: never;
+
+enum TopAskingResult {
+	OK,
+	NO_ELEMENT,
+	INVALID_PAGE
+}
+
+export enum TopKind {
+	SCORE_ALL_TIME = "score_all_time",
+	SCORE_WEEKLY = "score_weekly",
+	GLORY = "glory",
+	GUILDS = "guilds"
+}
+
+const ELEMENTS_PER_PAGE: Record<TopKind, number> = {
+	[TopKind.SCORE_ALL_TIME]: 15,
+	[TopKind.SCORE_WEEKLY]: 15,
+	[TopKind.GLORY]: 15,
+	[TopKind.GUILDS]: 15
+};
+
+export function getTopKind(dataType: TopDataType, timing: TopTiming): TopKind {
+	if (dataType === TopDataType.SCORE) {
+		return timing === TopTiming.ALL_TIME ? TopKind.SCORE_ALL_TIME : TopKind.SCORE_WEEKLY;
+	} else if (dataType === TopDataType.GLORY) {
+		return TopKind.GLORY;
+	} else {
+		return TopKind.GUILDS;
+	}
+}
+
+export function getTopPacket<T extends TopKind>(response: CrowniclesPacket[], result: AskTopResult<T>): void {
+	if (result.result === TopAskingResult.INVALID_PAGE) {
+		response.push(makePacket(CommandTopInvalidPagePacket, result.data));
+	} else if (result.result === TopAskingResult.NO_ELEMENT) {
+		if (result.kind === TopKind.GUILDS) {
+			response.push(makePacket(CommandTopGuildsEmptyPacket, result.data));
+		} else {
+			response.push(makePacket(CommandTopPlayersEmptyPacket, result.data));
+		}
+	} else {
+		if (result.kind === TopKind.SCORE_ALL_TIME || result.kind === TopKind.SCORE_WEEKLY) {
+			response.push(makePacket(CommandTopPacketResScore, result.data as TopObjectResponse<TopElementScore>));
+		} else if (result.kind === TopKind.GUILDS) {
+			response.push(makePacket(CommandTopPacketResGuild, result.data as TopObjectResponse<TopElementGuild>));
+		} else {
+			response.push(makePacket(CommandTopPacketResGlory, result.data as TopObjectResponse<TopElementGlory>));
+		}
+	}
+}
+
+function initialiseTopObjectStorage<Kind extends TopKind>(): TopObjectStorage<TopElementKind<Kind>> {
+	// These values won't be used, but we need to return something that matches the type
+	return {
+		totalElements: 0,
+		timing: TopTiming.ALL_TIME,
+		elements: []
+	};
+}
+
+export class TopStorage {
+	private static _instance: TopStorage;
+	private _tops: {
+		[key in TopKind]: TopObjectStorage<TopElementKind<key>>
+	} = {
+		[TopKind.SCORE_ALL_TIME]: initialiseTopObjectStorage<TopKind.SCORE_ALL_TIME>(),
+		[TopKind.SCORE_WEEKLY]: initialiseTopObjectStorage<TopKind.SCORE_WEEKLY>(),
+		[TopKind.GLORY]: initialiseTopObjectStorage<TopKind.GLORY>(),
+		[TopKind.GUILDS]: initialiseTopObjectStorage<TopKind.GUILDS>()
+	}
+	private static _topUpdateFunctions: {
+		[key in TopKind]: (now: number) => Promise<void>
+	} = {
+		[TopKind.SCORE_ALL_TIME]: async (now: number) => {
+			const totalElements = await Players.getNumberOfPlayingPlayers(false);
+			const elements = await Players.getPlayersTop(1, totalElements, false);
+			TopStorage.getInstance()._tops[TopKind.SCORE_ALL_TIME] = {
+				totalElements,
+				timing: TopTiming.ALL_TIME,
+				elements: elements.map((player, index) => ({
+					id: player.id,
+					rank: index + 1,
+					sameContext: false,
+					text: player.keycloakId,
+					attributes: {
+						1: {
+							effectId: player.currentEffectFinished(new Date(now)) ? undefined : player.effectId,
+							mapType: TravelTime.getTravelDataSimplified(player, new Date(now)).travelEndTime > now ? undefined : player.getDestination()?.type,
+							afk: player.isInactive()
+						},
+						2: player.score,
+						3: player.level
+					}
+				}))
+			};
+		},
+		[TopKind.SCORE_WEEKLY]: async (now: number) => {
+			const totalElements = await Players.getNumberOfPlayingPlayers(true);
+			const elements = await Players.getPlayersTop(1, totalElements, true);
+			TopStorage.getInstance()._tops[TopKind.SCORE_WEEKLY] = {
+				totalElements,
+				timing: TopTiming.WEEK,
+				elements: elements.map((player, index) => ({
+					id: player.id,
+					rank: index + 1,
+					sameContext: false,
+					text: player.keycloakId,
+					attributes: {
+						1: {
+							effectId: player.currentEffectFinished(new Date(now)) ? undefined : player.effectId,
+							mapType: TravelTime.getTravelDataSimplified(player, new Date(now)).travelEndTime > now ? undefined : player.getDestination()?.type,
+							afk: player.isInactive()
+						},
+						2: player.weeklyScore,
+						3: player.level
+					}
+				}))
+			};
+		},
+		[TopKind.GLORY]: async () => {
+			const totalElements = await Players.getNumberOfFightingPlayers();
+			const elements = await Players.getPlayersGloryTop(1, totalElements);
+			TopStorage.getInstance()._tops[TopKind.GLORY] = {
+				totalElements,
+				timing: TopTiming.WEEK,
+				elements: elements.map((player, index) => ({
+					id: player.id,
+					rank: index + 1,
+					sameContext: false,
+					text: player.keycloakId,
+					attributes: {
+						1: player.getLeague().id,
+						2: player.getGloryPoints(),
+						3: player.level
+					}
+				}))
+			};
+		},
+		[TopKind.GUILDS]: async () => {
+			const totalElements = await Guilds.getTotalRanked();
+			const elements = await Guilds.getRankedGuilds(1, totalElements);
+			TopStorage.getInstance()._tops[TopKind.GUILDS] = {
+				totalElements,
+				timing: TopTiming.ALL_TIME,
+				elements: elements.map((guild, index) => ({
+					id: guild.id,
+					rank: index + 1,
+					sameContext: false,
+					text: guild.name,
+					attributes: {
+						1: guild.score,
+						2: guild.level,
+						3: undefined
+					}
+				}))
+			};
+		}
+	};
+	private _cachedPositions: Map<TopKind, Map<number, number>> = new Map();
+	private _cachedPages: {
+		[key in TopKind]: Map<number, TopElementBaseStorage<TopElementKind<key>>[]>
+	} = {
+		[TopKind.SCORE_ALL_TIME]: new Map(),
+		[TopKind.SCORE_WEEKLY]: new Map(),
+		[TopKind.GLORY]: new Map(),
+		[TopKind.GUILDS]: new Map()
+	}
+	private _now: number = Date.now();
+
+	public static getInstance(): TopStorage {
+		if (!this._instance) {
+			this._instance = new TopStorage();
+		}
+		return this._instance;
+	}
+
+	public async updateTops() {
+		const now = Date.now();
+		for (const kind of Object.values(TopKind)) {
+			await TopStorage._topUpdateFunctions[kind](now);
+		}
+		this._cachedPositions.forEach((cachedPosition) => {
+			cachedPosition.clear();
+		});
+		for (const cachedPage of Object.values(this._cachedPages)) {
+			cachedPage.clear();
+		}
+		this._now = now;
+	}
+
+	public askTop<T extends TopKind>(kind: T, id: number, page: number, needFight?: number): AskTopResult<T> {
+		const top = this._tops[kind] as TopObjectStorage<TopElementKind<T>>;
+		if (!top) {
+			throw new Error(`Top of kind ${kind} not found`);
+		}
+		const {
+			totalElements,
+			timing,
+			elements
+		} = top;
+		const elementsPerPage = ELEMENTS_PER_PAGE[kind];
+		if (totalElements === 0) {
+			return {
+				result: TopAskingResult.NO_ELEMENT,
+				data: kind == TopKind.GLORY ? {needFight} : {},
+				kind
+			};
+		}
+		const maxPage = Math.ceil(totalElements / elementsPerPage);
+		if (page < 1 || page > maxPage) {
+			return {
+				result: TopAskingResult.INVALID_PAGE,
+				data: {
+					minPage: 1,
+					maxPage
+				},
+				kind
+			};
+		}
+		const minRank = (page - 1) * elementsPerPage + 1;
+		const maxRank = Math.min(page * elementsPerPage, totalElements);
+		let rank = this._cachedPositions.get(kind)!.get(id);
+		if (rank === undefined) {
+			rank = needFight && needFight > 0 ? -1 : elements.find(element => element.id === id)?.rank ?? -1;
+			this._cachedPositions.get(kind)!.set(id, rank);
+		}
+		let tmpElementPortion = this._cachedPages[kind].get(page);
+		let elementPortion: TopElementBaseStorage<TopElementKind<T>>[];
+		if (!tmpElementPortion) {
+			elementPortion = elements.filter(element => element.rank >= minRank && element.rank <= maxRank);
+			this._cachedPages[kind].set(page, elementPortion);
+		} else {
+			elementPortion = tmpElementPortion;
+		}
+		return {
+			result: TopAskingResult.OK,
+			now: this._now,
+			kind,
+			data: {
+				totalElements,
+				timing,
+				minRank,
+				maxRank,
+				contextRank: rank > 0 ? rank : undefined,
+				canBeRanked: kind !== TopKind.GUILDS || id !== null,
+				needFight,
+				elements: elementPortion.map(element => ({
+						rank: element.rank,
+						sameContext: element.id === id,
+						text: element.text,
+						attributes: element.attributes
+					})),
+				elementsPerPage
+			}
+		} as AskTopResult<T>;
+	}
+}
--- Lib/src/types/TopElement.ts
+++ Lib/src/types/TopElement.ts
@@ -2,6 +2,10 @@ export type TopElementScoreFirstType = {
 	effectId?: string; mapType?: string; afk: boolean;
 };
 
+export type TopElementScore = TopElement<TopElementScoreFirstType, number, number>;
+export type TopElementGlory = TopElement<number, number, number>;
+export type TopElementGuild = TopElement<number, number, undefined>;
+
 export interface TopElement<T, U, V> {
 	rank: number;
 
