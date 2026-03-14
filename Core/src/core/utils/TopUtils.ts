f40936158b8b699bc9e4291f1e3bcf3fa7a9004a
BastLast <lastaccount@fastmail.com>
2026-03-14 19:14:29 +0100

    add pagination to top command #3289

--- Core/src/core/utils/TopUtils.ts
+++ Core/src/core/utils/TopUtils.ts
@@ -11,7 +11,6 @@ import {
 } from "../../../../Lib/src/packets/CrowniclesPacket";
 import {
 	CommandTopGuildsEmptyPacket,
-	CommandTopInvalidPagePacket,
 	CommandTopPacketResGlory,
 	CommandTopPacketResGuild,
 	CommandTopPacketResScore,
@@ -34,10 +33,9 @@ type TopObjectResponse<Element> = TopObject & {
 	elements: Element[];
 	contextRank?: number;
 	canBeRanked: boolean;
-	minRank: number;
-	maxRank: number;
 	elementsPerPage: number;
 	needFight?: number;
+	initialPage?: number;
 };
 
 type AskTopResultBase = {
@@ -59,14 +57,7 @@ type AskTopResultNoElement = AskTopResultBase & {
 	data: { needFight?: number };
 };
 
-type AskTopResultInvalidPage = AskTopResultBase & {
-	result: TopAskingResult.INVALID_PAGE;
-	data: {
-		minPage: number; maxPage: number;
-	};
-};
-
-type AskTopResult<T extends TopKind> = AskTopResultOK<T> | AskTopResultNoElement | AskTopResultInvalidPage;
+type AskTopResult<T extends TopKind> = AskTopResultOK<T> | AskTopResultNoElement;
 
 type TopElementKind<Kind extends TopKind> = Kind extends TopKind.SCORE_ALL_TIME | TopKind.SCORE_WEEKLY
 	? TopElementScore
@@ -78,8 +69,7 @@ type TopElementKind<Kind extends TopKind> = Kind extends TopKind.SCORE_ALL_TIME 
 
 enum TopAskingResult {
 	OK,
-	NO_ELEMENT,
-	INVALID_PAGE
+	NO_ELEMENT
 }
 
 export enum TopKind {
@@ -113,10 +103,7 @@ export function getTopPacket<T extends TopKind>(response: CrowniclesPacket[], re
 		: result.kind === TopKind.GLORY
 			? [CommandTopPacketResGlory, CommandTopPlayersEmptyPacket]
 			: [CommandTopPacketResGuild, CommandTopGuildsEmptyPacket];
-	if (result.result === TopAskingResult.INVALID_PAGE) {
-		response.push(makePacket(CommandTopInvalidPagePacket, result.data));
-	}
-	else if (result.result === TopAskingResult.NO_ELEMENT) {
+	if (result.result === TopAskingResult.NO_ELEMENT) {
 		response.push(makePacket(packetKind[1], result.data));
 	}
 	else {
@@ -197,15 +184,6 @@ export class TopStorage {
 		[TopKind.GUILDS, new Map()]
 	]);
 
-	private _cachedPages: {
-		[key in TopKind]: Map<number, TopElementBaseStorage<TopElementKind<key>>[]>
-	} = {
-		[TopKind.SCORE_ALL_TIME]: new Map(),
-		[TopKind.SCORE_WEEKLY]: new Map(),
-		[TopKind.GLORY]: new Map(),
-		[TopKind.GUILDS]: new Map()
-	};
-
 	private _now: number = Date.now();
 
 	static topUpdateFunctionScore(weekly: boolean): (now: number) => Promise<void> {
@@ -249,14 +227,11 @@ export class TopStorage {
 		this._cachedPositions.forEach(cachedPosition => {
 			cachedPosition.clear();
 		});
-		for (const cachedPage of Object.values(this._cachedPages)) {
-			cachedPage.clear();
-		}
 		this._now = now;
 		CrowniclesLogger.info("Tops updated");
 	}
 
-	public askTop<T extends TopKind>(kind: T, id: number, page: number, needFight?: number): AskTopResult<T> {
+	public askTop<T extends TopKind>(kind: T, id: number, needFight?: number, initialPage?: number): AskTopResult<T> {
 		const top = this._tops[kind] as TopObjectStorage<TopElementKind<T>>;
 		if (!top) {
 			throw new Error(`Top of kind ${kind} not found`);
@@ -274,32 +249,10 @@ export class TopStorage {
 				kind
 			};
 		}
-		const maxPage = Math.ceil(totalElements / elementsPerPage);
-		if (page < 1 || page > maxPage) {
-			return {
-				result: TopAskingResult.INVALID_PAGE,
-				data: {
-					minPage: 1,
-					maxPage
-				},
-				kind
-			};
-		}
-		const minRank = (page - 1) * elementsPerPage + 1;
-		const maxRank = Math.min(page * elementsPerPage, totalElements);
 		let rank = this._cachedPositions.get(kind)!.get(id);
 		if (rank === undefined) {
 			rank = kind === TopKind.GLORY && needFight! > 0 ? -1 : elements.find(element => element.id === id)?.rank ?? -1;
 			this._cachedPositions.get(kind)!.set(id, rank);
-		}
-		const tmpElementPortion = this._cachedPages[kind].get(page);
-		let elementPortion: TopElementBaseStorage<TopElementKind<T>>[];
-		if (!tmpElementPortion) {
-			elementPortion = elements.filter(element => element.rank >= minRank && element.rank <= maxRank);
-			this._cachedPages[kind].set(page, elementPortion);
-		}
-		else {
-			elementPortion = tmpElementPortion;
 		}
 		return {
 			result: TopAskingResult.OK,
@@ -308,18 +261,17 @@ export class TopStorage {
 			data: {
 				totalElements,
 				timing,
-				minRank,
-				maxRank,
 				contextRank: rank > 0 ? rank : undefined,
 				canBeRanked: kind !== TopKind.GUILDS || id !== -1,
 				needFight,
-				elements: elementPortion.map(element => ({
+				elements: elements.map(element => ({
 					rank: element.rank,
 					sameContext: element.id === id,
 					text: element.text,
 					attributes: element.attributes
 				})),
-				elementsPerPage
+				elementsPerPage,
+				initialPage
 			}
 		} as AskTopResult<T>;
 	}
