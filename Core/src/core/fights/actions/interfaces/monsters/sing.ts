36468fc8dc87bb2a313e0c964050dca83580cd09
Ntalcme <57364571+Ntalcme@users.noreply.github.com>
2026-03-20 17:33:06 +0100

    Balancing sing attack using distance attack mec #3724

--- Core/src/core/fights/actions/interfaces/monsters/sing.ts
+++ Core/src/core/fights/actions/interfaces/monsters/sing.ts
@@ -2,11 +2,16 @@ import { FightAlterations } from "../../FightAlterations";
 import { FightActionFunc } from "../../../../../data/FightAction";
 import { simpleAlterationFightAction } from "../../templates/SimpleAlterationFightActionTemplate";
 import { RandomUtils } from "../../../../../../../Lib/src/utils/RandomUtils";
+import { FightActionType } from "../../../../../../../Lib/src/types/FightActionType";
 
-const use: FightActionFunc = (_sender, receiver) => {
+const use: FightActionFunc = (_sender, receiver, _fightAction) => {
+	const usedDistance = receiver.getLastFightActionUsed()?.type === FightActionType.DISTANCE;
+
 	const result = simpleAlterationFightAction(receiver, {
 		selfTarget: false,
-		alteration: RandomUtils.crowniclesRandom.bool(0.65) ? FightAlterations.SLEEPING : FightAlterations.CONFUSED
+		alteration: usedDistance
+			? RandomUtils.crowniclesRandom.bool(0.35) ? FightAlterations.SLEEPING : FightAlterations.CONFUSED
+			: RandomUtils.crowniclesRandom.bool(0.65) ? FightAlterations.SLEEPING : FightAlterations.CONFUSED
 	});
 
 	return {
