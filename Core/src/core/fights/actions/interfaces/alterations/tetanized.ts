ae25fd52909dc18c9bce2783835a7fd5697ad65a
Ntalcme <57364571+Ntalcme@users.noreply.github.com>
2026-04-11 20:17:41 +0200

    Mage & paladin buff on tetanized alte #4122

--- Core/src/core/fights/actions/interfaces/alterations/tetanized.ts
+++ Core/src/core/fights/actions/interfaces/alterations/tetanized.ts
@@ -20,10 +20,19 @@ function isPaladin(fighter: Fighter): boolean {
 		|| fighter.player.class === ClassConstants.CLASSES_ID.LUMINOUS_PALADIN;
 }
 
+function isMage(fighter: Fighter): boolean {
+	if (!(fighter instanceof PlayerFighter || fighter instanceof AiPlayerFighter)) {
+		return false;
+	}
+	return fighter.player.class === ClassConstants.CLASSES_ID.MYSTIC_MAGE;
+}
+
 const use: FightAlterationFunc = (affected, fightAlteration) => {
-	// Paladins shake off the fear more easily, other classes endure longer
-	const heroicHealChance = isPaladin(affected) ? 0.8 : 0.3;
-	if (affected.alterationTurn > 3 || (affected.alterationTurn > 1 && RandomUtils.crowniclesRandom.bool(heroicHealChance))) {
+	// Paladins shake off the fear more easily and have less nerf
+	// Mage is almost immunized but have full nerf
+	const affectedIsPaladin = isPaladin(affected);
+	const heroicHealChance = affectedIsPaladin ? 0.8 : 0.3;
+	if (isMage(affected) && RandomUtils.crowniclesRandom.bool(0.8) || (affected.alterationTurn > 3 || (affected.alterationTurn > 1 && RandomUtils.crowniclesRandom.bool(heroicHealChance)))) {
 		affected.removeAttackModifiers(fightAlteration);
 		affected.removeSpeedModifiers(fightAlteration);
 		affected.removeDefenseModifiers(fightAlteration);
@@ -31,14 +40,14 @@ const use: FightAlterationFunc = (affected, fightAlteration) => {
 	}
 
 	const result = defaultFightAlterationResult();
-
+	
 	if (!affected.hasAttackModifier(fightAlteration)) {
 		result.state = FightAlterationState.NEW;
 		FightActionController.applyBuff(result, {
 			selfTarget: true,
 			stat: FightStatBuffed.ATTACK,
 			operator: FightStatModifierOperation.MULTIPLIER,
-			value: 0.5
+			value: affectedIsPaladin ? 0.5 : 0.3
 		}, affected, fightAlteration);
 	}
 
@@ -47,7 +56,7 @@ const use: FightAlterationFunc = (affected, fightAlteration) => {
 			selfTarget: true,
 			stat: FightStatBuffed.SPEED,
 			operator: FightStatModifierOperation.MULTIPLIER,
-			value: 0.8
+			value: affectedIsPaladin ? 0.6 : 0.3
 		}, affected, fightAlteration);
 	}
 
@@ -56,7 +65,7 @@ const use: FightAlterationFunc = (affected, fightAlteration) => {
 			selfTarget: true,
 			stat: FightStatBuffed.DEFENSE,
 			operator: FightStatModifierOperation.MULTIPLIER,
-			value: 0.2
+			value: affectedIsPaladin ? 0.8 : 0.4
 		}, affected, fightAlteration);
 	}
 
