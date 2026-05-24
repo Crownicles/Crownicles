e319b2cc0621fce0c9b3c94f6f9ae8ee5c36df7d
BastLast <lastaccount@fastmail.com>
2026-05-24 19:50:08 +0200

    guard @packetHandler this binding via eslint rule + runtime proxy #4257

--- eslint-custom-rules/index.mjs
+++ eslint-custom-rules/index.mjs
@@ -4,10 +4,12 @@
 
 import singleLineShortSinglePropertyObject from "./single-line-short-single-property-object.mjs";
 import noUnguardedSave from "./no-unguarded-save.mjs";
+import noThisInPacketHandler from "./no-this-in-packet-handler.mjs";
 
 export default {
 	rules: {
 		"single-line-short-single-property-object": singleLineShortSinglePropertyObject,
-		"no-unguarded-save": noUnguardedSave
+		"no-unguarded-save": noUnguardedSave,
+		"no-this-in-packet-handler": noThisInPacketHandler
 	}
 };
