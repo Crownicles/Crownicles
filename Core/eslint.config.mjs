b0cc63ff791aa4b83234945efdeb1aa836c7b935
BastLast <lastaccount@fastmail.com>
2026-07-16 23:11:15 +0200

    fix /boire unbound handler #4432

--- Core/eslint.config.mjs
+++ Core/eslint.config.mjs
@@ -31,6 +31,7 @@ export default defineConfig([
 		rules: {
 			...customRules,
 			"crownicles/single-line-short-single-property-object": ["error", { maxLength: 40 }],
+			"crownicles/no-this-in-packet-handler": "error",
 			"crownicles/no-response-push-through-return": "error"
 		}
 	},
